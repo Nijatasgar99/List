e1edca785a35fde897b08c0db99031ded6da4459
Nijatasgar99 <nijataas@code.edu.az>
2021-10-25 20:52:19 +0500

    Добавьте файлы проекта.

---
+++ List/Program.cs
@@ -0,0 +1,35 @@
+﻿using List.collections;
+using System;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mylist<string> list = new mylist<string>();
+            list.Add("user1");
+            list.Add("user2");
+            list.Add("user3");
+            list.Add("user4");
+            list.Add("user5");
+            list.Add("user6");
+            list.Add("user7");
+            list.Add("user8");
+            list.Add("user9");
+            list.Add("user10");
+            list.Add("user11");
+            list.Add("user12");
+            list.Add("user13");
+            list.Add("user14");
+            list.Add("user15");
+            list.Add("user16");
+            mylist<int> list2 = new mylist<int>();
+            list2.Add(1);
+            list2.Add(2);
+            list2.Add(3);
+            Console.WriteLine(list.Contains("user2"));
+            //Console.WriteLine(list2.Find(i=>i%100==0));
+        }
+    }
+}
---
+++ List/collections/mylist.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List.collections
+{
+    public class mylist<T>
+    {
+        private T[] arr;
+        //private static int count;
+        public int Count { get; set; }
+        public mylist()
+        {
+           
+            arr = new T[10];
+        }
+        public void AddRange(IEnumerable<T> list)
+        {
+            foreach (var item in list)
+            {
+                Add(item);
+            }
+
+        }
+        public void Add(T obj)
+        {
+            if (Count%15==0)
+            {
+                arr[Count] = obj;
+                Count++;
+            }
+
+        }
+        private void ArrayResize()
+        {
+            Array.Resize(ref arr, arr.Length + 15); 
+        }
+        public void clear()
+        {
+            arr = new T[15];
+            Count = 0;
+        }
+        public T Find(Predicate<T> func)
+
+        {
+            
+            foreach (var item in arr)
+            {
+                if (func(item))
+                {
+                    return item;
+                }
+            }
+            return default;
+
+        }
+        public bool Contains(T obj)
+        {
+            foreach (var item in arr)
+            {
+                if (Comparer<T>.Default.Compare(item, obj)==0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
